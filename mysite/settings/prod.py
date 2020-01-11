80d06a100dcad41fec54947cd681b97e2140e647
yoongyo <jyg0172@naver.com>
2020-01-12 02:00:53 +0900

    Ad

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -21,7 +21,7 @@ def get_secret(setting, secret=secret):
 AWS_ACCESS_KEY_ID = get_secret('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = get_secret('AWS_SECRET_ACCESS_KEY')
 
-AWS_STORAGE_BUCKET_NAME = 'tesco'
+AWS_STORAGE_BUCKET_NAME = 'tescob'
 
 # AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.ap-northeast-2.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
